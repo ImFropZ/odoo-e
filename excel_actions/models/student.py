c173e7759a90394ac1036b7f66cda748a2e4164b
Lim Tangmeng <limtangmeng100@gmail.com>
2023-07-20 15:59:09 +0700

    HR MODULE

--- excel_actions/models/student.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, api, models
-
-
-class ExcelStudent(models.Model):
-    _name = "excel.student"
-
-    name = fields.Text(string="Fullname")
-    age = fields.Integer(string="Age")
